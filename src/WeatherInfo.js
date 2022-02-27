7bd9e55e0584a4c15e05959768bdc8d6c01b8266
Kerstin <kerstin_mayer@web.de>
2022-02-27 21:08:18 +0100

    integrated weather icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -36,11 +37,7 @@ export default function WeatherInfo(props) {
           </h4>
         </div>
         <div className="col-3">
-          <img
-            src={props.data.iconUrl}
-            alt={props.data.description}
-            id="icon"
-          />
+          <WeatherIcon code={props.data.icon} alt={props.data.description} />
         </div>
       </div>
     </div>
