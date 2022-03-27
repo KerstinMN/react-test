b28b8b65f0b763c386edb7b4a78fe3188b784b3f
Kerstin <kerstin_mayer@web.de>
2022-03-27 21:08:02 +0200

    fixed the forecast function by using useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
-import weatherForecastDay from "./WeatherForecastDay";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(
+    function () {
+      setLoaded(false);
+    },
+    [props.coordinates]
+  );
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -16,9 +23,17 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <weatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,63 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("Fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <h4>
-          <span className="temperature" id="temperature">
-            {Math.round(props.celsius)}
-          </span>
-          <span className="units">
-            <a href="/">°C</a> |
-            <a
-              href="/"
-              id="celsius-link"
-              className="active"
-              onClick={showFahrenheit}
-            >
-              F{" "}
-            </a>
-          </span>
-        </h4>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <h4>
-          <span className="temperature" id="temperature">
-            {Math.round(fahrenheit())}
-          </span>
-          <span className="units">
-            <a
-              href="/"
-              id="celsius-link"
-              className="active"
-              onClick={showCelsius}
-            >
-              °C
-            </a>{" "}
-            | F{" "}
-          </span>
-        </h4>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <h4>
+        <span className="temperature" id="temperature">
+          {Math.round(props.celsius)}
+        </span>
+        <span className="units">°C</span>
+      </h4>
+    </div>
+  );
 }
